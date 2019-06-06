89baaa290b6b08521750ee15f391b8fdad424de5
chenb0x <artofmorellc@gmail.com>
2019-06-06 14:33:56 -0700

    Code movement

--- TheRealChen.py
+++ TheRealChen.py
@@ -18,14 +18,16 @@ lastmsg = "i will fight you"
 
 @client.event
 async def on_ready():
+    with open("members.txt", "w") as m:
+        for guild in client.guilds:
+            for member in guild.members:
+                try:
+                    print(member)
+                    m.write(member)
+                except:
+                    pass
     print(f"Logged in as {client.user}")
     print(f"-------")
-    for guild in client.guilds:
-        for member in guild.members:
-            try:
-                print(member)
-            except:
-                pass
 
 @client.event
 async def on_message(message):
